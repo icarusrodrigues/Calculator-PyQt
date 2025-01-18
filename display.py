84a76e79aa0adab4e0ab459efb88dd99021a287b
Icarus Rodrigues <icaros1105@gmail.com>
2025-01-17 23:03:41 -0300

    Add validator to display text

--- display.py
+++ display.py
@@ -1,11 +1,17 @@
 from PySide6.QtWidgets import QLineEdit
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QLocale
+from PySide6.QtGui import QDoubleValidator
 from variables import BIG_FONT_SIZE, TEXT_MARGIN, MINIMUM_WIDTH
 
 class Display(QLineEdit):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.configStyle()
+
+        validator = QDoubleValidator()
+        validator.setNotation(QDoubleValidator.Notation.StandardNotation)
+        validator.setLocale(QLocale(QLocale.c()))
+        self.setValidator(validator)
 
     def configStyle(self):
         self.setMinimumWidth(MINIMUM_WIDTH)
